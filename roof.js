20f2c9f4d79e1e8a4806cd9126f42fb3c39d2862
KarthikTHECoder25 <71226901+KarthikTHECoder25@users.noreply.github.com>
2021-06-28 09:18:06 -0400

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,23 @@
+class roof{
+    constructor(x,y,w,h){
+        var options={
+            isStatic: true
+
+        }
+        this.x = x
+        this.w = w
+        this.y = y
+        this.h = h
+        this.body = Bodies.rectangle(x,y,w,h,options)
+        World.add(world,this.body) 
+    }
+    display(){
+        var pos = this.body.position
+        push()
+        translate(pos.x,pos.y)
+        rectMode(CENTER)
+        fill("brown")
+        rect(0,0,this.w,this.h)
+        pop()
+    }
+} 
